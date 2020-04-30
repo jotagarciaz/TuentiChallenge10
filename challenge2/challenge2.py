3eab16d69af695855e6bc528afddfdbf9a510912
Quini Macbook <quini.emancipia@gmail.com>
2020-04-30 11:50:45 +0200

    challenge 2 refactored

--- challenge2/challenge2.py
+++ challenge2/challenge2.py
@@ -1,26 +1,41 @@
+# In this case we have to return the winner of a number of matches N
+# We will have also a number of group of matches C, like a kind of tournament
+# So for resolve this, we will have a dictionary scores with the players' numbers as a key and the number of victories as a value
+# if we receive 1 in the third parameter of a line, then player of the first parameter wins else player of the second parameter wins.
+
 import operator
 
-
-
 results=""
-with open('test') as f:
+
+with open('Input.txt') as f:
     C = int(f.readline())
     
     i = 1
     for j in range(C):
         scores = {}
         N = int(f.readline())
+        
         for _ in range(N):
             line = f.readline()
-            aux= line.split(" ")
-            score_aux_1 = scores.get(aux[0],0)
-            score_aux_2 = scores.get(aux[1],0)
+            
+            playersAndScore = line.split(" ")
            
-            if int(aux[2]) == 1:
-                scores.update({aux[0]:score_aux_1+1})
-            else:
-                scores.update({aux[1]:score_aux_2+1})
+            
+            player1 = playersAndScore[0]
+            player2 = playersAndScore[1]
+            wins = int(playersAndScore[2])
 
+            score_player1 = scores.get(player1,0)
+            score_player2 = scores.get(player2,0)
+
+            if wins == 1:
+                # number of victories incresed in 1 for the player 1
+                scores.update({player1:score_player1+1})
+            else:
+                # number of victories incresed in 1 for the player 2
+                scores.update({player2:score_player2+1})
+        
+        # return the max score in the dictionary
         results += f"Case #{i}: {max(scores.items(), key=operator.itemgetter(1))[0]}\n"
         i += 1
 
