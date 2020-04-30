500a661284b44310d42287f5744db10b886a5448
Quini Macbook <quini.emancipia@gmail.com>
2020-04-30 23:12:43 +0200

    tuentistic pass  🥰

--- Challenge5/challenge5.py
+++ Challenge5/challenge5.py
@@ -1,27 +1,28 @@
+#To execute this code first we have to install more_itertools, for it we need to execute: ` pip install more-itertools ` in a terminal
+#For this challenge I will receive a number of cases (C) and numbers, that I should check if they are tuntistic or can be done by adding tuentistics.
+#At last I have to return the maximun possible of elements of its Tuentistic sums.  
+#I checked that the only numbers that are not tuentistics and can't be done by sums are: 
+#the ones from 0 to 19, the ones from 30 to 39 and the last one Untuentistic is 59, so I only have to check if the input is or not one of those
+#Also the maximun number of possible sums are the combinations of the little tuentistic numbers (from 20 to 29), so I have only to check how many times a number is divided by 20 
+
 from more_itertools import locate
 
+
 results=""
-with open('test') as f:
+with open('Input.txt') as f:
     C = int(f.readline())
     
-    for j in range(1,C+1):
-        line_text = f.readline().rstrip()
+    for case in range(1,C+1):
+        lineText = f.readline().rstrip() #get text and remove empty character
+        caseNumber = int(lineText)
+        zeroToTwenty = list(range(0,20)) #list that contains number from 0 to 19
+        noPossiblyTuentistics = zeroToTwenty + [30,31,32,33,34,35,36,37,38,39,59] #list with all Untuentistic numbers
         
-        """list_of_twos = list(locate(line_text, lambda a: a == '2')) #to get all the positions where there is a 2
-        list_of_twos = map(lambda x: len(line_text)-1-x, list_of_twos)
-        #print(list(list_of_twos))
-        list_of_twos = list(map(lambda x: x % 3 == 1 , list_of_twos)) #two obtain if the position of the two is one of the called tuentistic: 20, 20000, 20000000, 20000000000
-        
-        if True in list_of_twos:
-            results += f"Case #{j}: {line_text}\n"
-        else:"""
-        line_number = int(line_text)
-        zero_two_twenty = list(range(0,20))
-        no_possible_tuentistic = zero_two_twenty + [30,31,32,33,34,35,36,37,38,39,59]
-        if line_number in no_possible_tuentistic:
-            results += f"Case #{j}: IMPOSSIBLE\n"
+        if caseNumber in noPossiblyTuentistics:
+            results += f"Case #{case}: IMPOSSIBLE\n"
         else:
-            results += f"Case #{j}: {int(line_number/20)}\n"
+            maxNumberOfElements = int(caseNumber//20)
+            results += f"Case #{case}: {maxNumberOfElements}\n"
 
 
 
